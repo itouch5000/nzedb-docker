05658987d7556601c54d131a21febd3c49516568
Peter Butler <butlerpeter@hotmail.com>
2016-06-09 15:38:09 +0100

    Updating to support changes to nZEDb since last update

--- nZEDb.sh
+++ nZEDb.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
+export COMPOSER_HOME=/root
+export TERM=xterm
+
 chmod -R 777 /var/lib/php5
 
-if [[ ! -e /var/www/nZEDb/www/config.php ]]; then
+cd /tmp
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
 
-  echo "config.php does not exist. Cloning nZEDb..."
-  mkdir /var/www
-  cd /var/www
-  git clone https://github.com/nZEDb/nZEDb.git
-  chown www-data:www-data nZEDb/www -R
-  chmod 777 /var/www/nZEDb/libs/smarty/templates_c
+#if [[ ! -e /var/www/nZEDb/www/config.php ]]; then
 
-  chmod -R 777 /var/www/nZEDb/resources/covers/
-  chmod -R 777 /var/www/nZEDb/resources/nzb/
-  chmod -R 777 /var/www/nZEDb/resources/tmp/unrar/
+#  echo "config.php does not exist. Cloning nZEDb..."
+#  mkdir /var/www
+#  cd /var/www
+#  git clone https://github.com/nZEDb/nZEDb.git
+#  chown www-data:www-data nZEDb/www -R
+#  chmod 777 /var/www/nZEDb/libs/smarty/templates_c
 
-else
+#  chmod -R 777 /var/www/nZEDb/resources/covers/
+#  chmod -R 777 /var/www/nZEDb/resources/nzb/
+#  chmod -R 777 /var/www/nZEDb/resources/tmp/unrar/
 
-  export TERM=xterm
+#else
+
+if [[ -e /var/www/nZEDb/nzedb/config/config.php ]]; then
+
   cd /var/www/nZEDb/misc/update/nix/tmux
   exec php start.php
 
